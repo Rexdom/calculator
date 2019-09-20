8472f9d8a0db9caeef6f2da8be05ec6ffc0c88d0
Rexdom <rexdom123@hotmail.com>
2019-09-20 12:54:14 +0800

    Created add user form and calculation form

---
+++ src/AddNewUser.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function AddNewUser(props) {
+    const [name, setName] = useState('');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        props.onAdd(name);
+        setName('');
+
+    };
+    return (
+        <>
+            <p>Please add all the names of your groupmates below to begin:</p>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder="Name of your groupmate" 
+                />
+                <button>Add</button>
+            </form>
+        </>
+    );
+}
+
+export default AddNewUser;
