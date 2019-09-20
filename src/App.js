e56937271e13c8c0f40a6ee68ac055e7aa976a8b
Rexdom <rexdom123@hotmail.com>
2019-09-20 12:59:38 +0800

    Modified App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddNewUser from './AddNewUser';
+import CalculationForm from './CalculationForm';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  function addUser(user) {
+    setUserList([...userList,user])
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddNewUser onAdd={addUser}/>
+        <CalculationForm list={userList}/>
       </header>
     </div>
   );
