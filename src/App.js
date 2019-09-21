435a45c5ba4de941efe40fe8f5d75dec6f9dd7b7
Rexdom <rexdom123@hotmail.com>
2019-09-21 17:46:43 +0800

    Finish the structure of form

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AddNewUser from './AddNewUser';
-import CalculationForm from './CalculationForm';
+import FormList from './FormList';
 
 function App() {
   const [userList, setUserList] = useState([]);
   function addUser(user) {
-    setUserList([...userList,user])
+    setUserList(preList=>[...preList,user])
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <AddNewUser onAdd={addUser}/>
-        <CalculationForm list={userList}/>
+        <FormList list={userList}/>
       </header>
     </div>
   );
