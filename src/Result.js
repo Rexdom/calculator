b84f687d6a1225ef13be64b166c8567c75e13156
Leo Cheuk <rexdom123@hotmail.com>
2023-07-31 18:59:16 -0700

    update result section amount display

--- src/Result.js
+++ src/Result.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function Result(props){
-    return (
-        <>
-            <ul className="result">
-                {Object.keys(props.result).map(user=>
-                    <li key={user}>{user} should {props.result[user]>0?"take":"pay"} {Math.abs(props.result[user])} dollars</li>
-                )}
-            </ul> 
-            <button onClick={props.onBack}>Back</button>
-        </>
-    )
-};
+function Result(props) {
+  return (
+    <>
+      <ul className="result">
+        {Object.keys(props.result).map((user) => (
+          <li key={user}>
+            {user} should {props.result[user] > 0 ? "take" : "pay"}{" "}
+            {Math.round(Math.abs(props.result[user]) * 100) / 100} dollars
+          </li>
+        ))}
+      </ul>
+      <button onClick={props.onBack}>Back</button>
+    </>
+  );
+}
 
 export default Result;
